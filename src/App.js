4889bcee14cec183063d930ce6cd07d0bcd529a5
Vinicios Neves <vinny@macbook.local>
2021-03-10 11:18:39 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom'
+import Mage from './components/Mage'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        ReactDOM.createPortal(
+        <Mage />,
+        document.getElementById('thanos-location')
+        )
+      }
     </div>
   );
 }
---
+++ src/components/Mage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Mage = () => (
+  <>
+    <h3>Doctor</h3>
+    <h4>Strange</h4>
+  </>
+)
+
+export default Mage
